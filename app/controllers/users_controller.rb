4771272611561b9dde4eb0b0bab8f05d075ade22
Luis Buenaventura <luis@syndeolabs.com>
2014-05-17 12:40:26 +0800

    added count limiter in instagram request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
   def select_photos
     if client = Instagram.client(:access_token => current_user.access_token)
-      @photos = client.user_recent_media
+      @photos = client.user_recent_media(count: 30)
     else
       flash[:alert] = "Please login to Instagram again."
       sign_out_and_redirect current_user, root_path
